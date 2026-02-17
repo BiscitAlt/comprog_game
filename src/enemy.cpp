e57c3cd7b6df878e0f9588bcb51b089b3abb7dbb
GAME <naravitwongcome@gmail.com>
2026-02-17 19:22:51 +0700

    enemy.cpp

--- src/enemy.cpp
+++ src/enemy.cpp
@@ -1,17 +1,23 @@
 #include "enemy.h"
 #include "raymath.h"
 
-void InitEnemy(Enemy& e, Vector2 pos)
+
+void InitEnemy(Enemy& e, Vector2 pos,std::string name, int hp, int atk)
 {
     e.pos = pos; // กำหนดจุดเกิดเริ่มต้นของศัตรู
     e.size = { 18, 18 }; // ขนาดของศัตรู
     e.speed = 1.0f; // ความเร็ว
-    e.hp = 1; // เลือด
+    e.hp = hp; // เลือด
     e.color = BLUE;
+    e.namemonster = name;
+    e.hp = hp;
+    e.maxHp = hp;   // ตั้งค่าเลือดสูงสุดให้เท่ากับเลือดที่เกิดมาก่อนยังไม่โดนตี
+    e.attack = atk;
 }
 
 void UpdateEnemy(Enemy& e, Vector2 playerPos)
 {
+    if (e.hp <= 0) return;
     Vector2 dir = Vector2Subtract(playerPos, e.pos); // 
 
     if (Vector2Length(dir) > 1) 
