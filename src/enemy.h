3b725bc56b083920b1b2a7303996175c0ed9eb50
GAME <naravitwongcome@gmail.com>
2026-02-17 19:24:52 +0700

    enemy.h

--- src/enemy.h
+++ src/enemy.h
@@ -5,19 +5,25 @@
 #include "raylib.h"
 #include "map.h"
 #include "item.h"
-
+#include <string>
+#include <vector>
 //คร่าวๆ 
 struct Enemy
 {
     Vector2 pos; 
     Vector2 size;
     float speed;
-    int hp;
     Color color;
+    std::string namemonster;
+    int maxHp;
+    int attack;
+    int hp;
 };
 
+
+
 // ประกาศฟังก์ชัน
-void InitEnemy(Enemy& e, Vector2 pos); // ค่าพื้นฐาน
+void InitEnemy(Enemy& e, Vector2 pos ,std::string name, int hp, int atk); // ค่าพื้นฐาน
 void UpdateEnemy(Enemy& e, Vector2 playerPos); 
 void DrawEnemy(const Enemy& e); 
 
