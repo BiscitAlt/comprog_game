363bd4824aa757c39ccf147f0d8ee33dcd0b5326
BiscitAlt <thitiputt.pe@gmail.com>
2026-03-07 00:09:14 +0700

    Revert "Merge branch 'main' of https://github.com/BiscitAlt/comprog_game into map-core"
    This reverts commit 86b7a70e8e06ec2528ee268b7e691fc9e66bccb0, reversing
    changes made to 01e4710150560b566f91354fdadcbeac456a8466.

--- src/item.h
+++ src/item.h
@@ -1,19 +1,13 @@
 #ifndef ITEM_H
 #define ITEM_H
 
+// รวม header ไม่ใส่ใน .cpp เพราะจะได้ไม่ต้อง include ซ้ำหลายๆ ที่
 #include "raylib.h"
 
-struct Gem {
-    Vector2 pos;
-    int value;
-    bool active;
-    Color color;
-};
-
 struct Box {
-    Rectangle rect;
+    Rectangle rect; // เก็บทั้ง x, y, width, height ในตัวเดียว
     Color color;
-    bool isSolid;
+    bool isSolid;   // ถ้า true คือเดินทะลุไม่ได้
 };
 
 #endif
--- src/player.h
+++ src/player.h
@@ -1,6 +1,7 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 
+// รวม header ไม่ใส่ใน .cpp เพราะจะได้ไม่ต้อง include ซ้ำหลายๆ ที่
 #include "raylib.h" 
 #include "map.h"
 #include "item.h"
@@ -13,31 +14,28 @@ struct Skill {
     int mpCost;
 };
 
-struct player {
+
+struct player
+{
     Vector2 pos;
     Vector2 size;
     float speed;
-    float stamina;      
     Color color;
     std::string name;
-    
     int hp;
-    int hpMax;
     int maxHp;    
-    
     int mp;
     int maxMp;    
     int attack;
-    
-    int level;          
-    int exp;
-    int expNext;
-    
     std::vector<Skill> skillList;
 };
 
-void plUpdate(player &pl, Map &map);
+
+
+
+
 void plMovement(Vector2 &plPos, float speed);
 void plCollision(Vector2 &plPos, Vector2 plSize, float plSpeed, Map &map);
 
 #endif
+
